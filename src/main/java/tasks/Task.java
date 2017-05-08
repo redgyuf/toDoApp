7cbd344f2dcf383bb856efa9ae1545cf4ef18762
redgyuf <faramirex070@gmail.com>
2017-05-08 10:54:11 +0200

    Task basic structure implemented

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,5 +1,27 @@
 package tasks;
 
 public class Task {
+	private String name;
+	private TaskStatus status;
+	
+	public Task(String name) {
+		super();
+		this.name = name;
+		status = TaskStatus.ACTIVE;
+	}
 
+	public Task(String name, TaskStatus status) {
+		super();
+		this.name = name;
+		this.status = status;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public TaskStatus getStatus() {
+		return status;
+	}
+	
 }
