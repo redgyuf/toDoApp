243354ff089bb4625365bfca55282f5ec8bb2da4
redgyuf <faramirex070@gmail.com>
2017-05-09 11:05:52 +0200

    reloading tasks after remove now works

--- WebContent/js/todo.js
+++ WebContent/js/todo.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
-//	$("#getList").click(function (e) {
-//		e.preventDefault();
-//		console.log(this.id);
-//		
-//		//btn.onClick = (event) => console.log(asd);
-		
-		$.get("./TaskManager", "asd", function (response) {
-			console.log(response);
-			for(task of response){
-				$("#toDoList").append('<li class="list-group-item">' + task.name + '<button id=' + task.id + ' type="button" class="btn btn-xs btn-danger" onClick="removeTask('+ task.id + ')">Remove</button> <button id=' + task.id + ' type="button" class="btn btn-xs btn-success">Done</button></li>');
-			}			
-		});
-//	});	
+	getTasks();
 });
+
+function getTasks(){
+	$.get("./TaskManager", "asd", function (response) {
+		console.log(response);
+		$("#toDoList").html("");
+		for(task of response){
+			$("#toDoList").append('<li class="list-group-item">' + task.name + '<button id=' + task.id + ' type="button" class="btn btn-xs btn-danger" onClick="removeTask('+ task.id + ')">Remove</button> <button id=' + task.id + ' type="button" class="btn btn-xs btn-success">Done</button></li>');
+		}			
+	});
+}
 
 function removeTask(id){
 	$.ajax({
@@ -21,5 +19,5 @@ function removeTask(id){
 	    success: function(result) {	        
 	    }
 	});
-	console.log(id);
+	getTasks();
 }
