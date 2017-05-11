ed5be5a30fb3b6eacaee02e3ca7fa11ca39ddf25
redgyuf <faramirex070@gmail.com>
2017-05-11 11:15:37 +0200

    deadcode removed

--- src/main/java/tasks/TaskStatus.java
+++ src/main/java/tasks/TaskStatus.java
@@ -2,12 +2,5 @@ package tasks;
 
 public enum TaskStatus {
 	ACTIVE,
-	COMPLETED;
-	
-	public static void main(String[] args) {
-		String s = "ACTIVE";
-		TaskStatus ts = TaskStatus.valueOf(s);
-		String s2 = ts.name();
-		System.out.println();
-	}
+	COMPLETED
 }
